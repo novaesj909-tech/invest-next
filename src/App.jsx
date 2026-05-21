f2987998d21bd7db47aa3d0d0084e83a72101e39
Jefferson Novaes <novaesj909@gmail.com>
2026-05-21 18:26:34 -0300

    checkout produtos

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,19 @@ export default function InvestNext() {
     nome: "Guia Renda com IA",
     preco: "R$ 29,90",
     desc: "Aprenda formas simples de usar IA para criar renda digital.",
-    link: "https://wa.me/5511968882732"
+    link: "https://pay.kiwify.com.br/cbZqb32"
   },
   {
     nome: "Mentoria Start Digital",
     preco: "R$ 97",
     desc: "Plano prático para criar sua primeira estrutura online.",
-    link: "https://wa.me/5511968882732"
+    link: "https://pay.kiwify.com.br/cbZqb32"
   },
   {
     nome: "Automação com IA",
     preco: "R$ 197",
     desc: "Criação de chatbot, site e captação automática de clientes.",
-    link: "https://wa.me/5511968882732"
+    link: "https://pay.kiwify.com.br/cbZqb32"
   }
 ]
   return (
